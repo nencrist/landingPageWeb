da634998c3b47dd0467d4ca423c8003f90c23fac
Nicolas Medina P <nickmedina99@hotmail.com>
2020-09-28 07:02:39 -0500

    se intento arreglar lo de las rutas, no se pudo

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,34 +5,34 @@ const interactiveLogo = document.querySelector('.image__interactiveLogo')
 function colorHandle(event) {
     var color = event.target.getAttribute('name');
     if(color == "black"){
-        interactiveImage.setAttribute('src', "../Images/persoCapBlack.png");
+        interactiveImage.setAttribute('src', "./Images/persoCapBlack.png");
     }
     if(color == "red"){
-        interactiveImage.setAttribute('src', "../Images/persoCapRed.png");
+        interactiveImage.setAttribute('src', "./Images/persoCapRed.png");
     }
     if(color == "blue"){
-        interactiveImage.setAttribute('src', "../Images/persoCapBlue.png");
+        interactiveImage.setAttribute('src', "./Images/persoCapBlue.png");
     }
     if(color == "green"){
-        interactiveImage.setAttribute('src', "../Images/persoCapGreen.png");
+        interactiveImage.setAttribute('src', "./Images/persoCapGreen.png");
     }
     if(color == "yellow"){
-        interactiveImage.setAttribute('src', "../Images/persoCapYellow.png");
+        interactiveImage.setAttribute('src', "./Images/persoCapYellow.png");
     }
     if(color == "orange"){
-        interactiveImage.setAttribute('src', "../Images/persoCapOrange.png");
+        interactiveImage.setAttribute('src', "./Images/persoCapOrange.png");
     }
     if(color == "brown"){
-        interactiveImage.setAttribute('src', "../Images/persoCapBrown.png");
+        interactiveImage.setAttribute('src', "./Images/persoCapBrown.png");
     }
     if(color == "white"){
-        interactiveImage.setAttribute('src', "../Images/persoCap.png");
+        interactiveImage.setAttribute('src', "./Images/persoCap.png");
     }
     if(color == "black2"){
-        interactiveLogo.setAttribute('src', "../Images/blackLogo.png")
+        interactiveLogo.setAttribute('src', "./Images/blackLogo.png")
     }
     if(color == "white2"){
-        interactiveLogo.setAttribute('src', "../Images/whiteLogo.png");
+        interactiveLogo.setAttribute('src', "./Images/whiteLogo.png");
     }
     console.log('click', color);
 }
