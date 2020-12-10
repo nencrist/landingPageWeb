4a2ffc7b6938a3fa82068937b05f6acae845e80a
Nicolas Medina P <nickmedina99@hotmail.com>
2020-12-10 18:13:43 -0500

    se termino la interaccion

--- Scripts/microinter.js
+++ Scripts/microinter.js
@@ -9,3 +9,82 @@ window.addEventListener('load', function(){
 
 });
 
+//interaccion popUp
+
+const btnOpenRed = document.querySelector('.redCap');
+const btnOpenBlue = document.querySelector('.blueCap');
+const btnOpenWhite = document.querySelector('.whiteCap');
+const btnOpenOrange = document.querySelector('.orangeCap');
+const overlay = document.querySelector('.overlay');
+const popUpRed = document.querySelector('.popUp');
+const popUpBlue = document.querySelector('.popUp--blue');
+const popUpWhite = document.querySelector('.popUp--white');
+const popUpOrange = document.querySelector('.popUp--orange');
+
+const closeRed = document.querySelector('.closePopUp');
+const closeBlue = document.querySelector('.closePopUp--blue');
+const closeWhite= document.querySelector('.closePopUp--white');
+const closeOrange = document.querySelector('.closePopUp--orange');
+
+
+
+btnOpenRed.addEventListener('click', function(event){
+
+    event.preventDefault();
+    overlay.classList.add('active');
+    popUpRed.classList.add('active');
+
+});
+btnOpenBlue.addEventListener('click', function(event){
+
+    event.preventDefault();
+    overlay.classList.add('active');
+    popUpBlue.classList.add('active');
+
+});
+btnOpenWhite.addEventListener('click', function(event){
+
+    event.preventDefault();
+    overlay.classList.add('active');
+    popUpWhite.classList.add('active');
+
+});
+btnOpenOrange.addEventListener('click', function(event){
+
+    event.preventDefault();
+    overlay.classList.add('active');
+    popUpOrange.classList.add('active');
+
+});
+
+closeRed.addEventListener('click', function(event){
+
+    event.preventDefault();
+    overlay.classList.remove('active');
+    popUpRed.classList.remove('active');
+});
+
+closeBlue.addEventListener('click', function(event){
+
+    event.preventDefault();
+    overlay.classList.remove('active');
+    popUpBlue.classList.remove('active');
+
+});
+
+
+closeWhite.addEventListener('click', function(event){
+
+    event.preventDefault();
+    overlay.classList.remove('active');
+    popUpWhite.classList.remove('active');
+
+});
+
+closeOrange.addEventListener('click', function(event){
+
+    event.preventDefault();
+    overlay.classList.remove('active');
+    popUpOrange.classList.remove('active');
+
+});
